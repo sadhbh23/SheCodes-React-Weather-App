f9baaed947a9cac16bcabfbaf47cd8f5d95a6d3a
sadhbh23 <127418826+sadhbh23@users.noreply.github.com>
2023-09-04 19:52:06 +0100

    Created Weather.css File & Altered Weather Component Styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./App.css";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://sadhbhredmond.dev/" target="_blank">
+            Sadhbh Redmond
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/sadhbh23/SheCodes-React-Weather-App"
+            target="_blank"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="container">
+      <div className="container main-content">
         <form>
           <div className="row">
             <div className="col-9">
@@ -15,7 +16,11 @@ export default function Weather() {
               />
             </div>
             <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary" />
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
         </form>
@@ -24,13 +29,15 @@ export default function Weather() {
           <li>Monday, 4 September</li>
           <li>Sunny</li>
         </ul>
-        <div className="row">
+        <div className="row mt-4">
           <div className="col-6">
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
               alt="Sunny"
             />
-            24°C
+
+            <span className="temperature-numeric-value">24</span>
+            <span className="temperature-unit">°C</span>
           </div>
           <div className="col-6">
             <ul>
